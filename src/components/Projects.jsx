59e9ca7f5a08cf19e2f8cd4e82e8fde2dc61c906
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2023-04-10 11:17:42 -0400

    styling changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ export default function Projects() {
     return(
         <div className="bg-gradient-to-r from-blue-300 to-blue-50 text-center h-screen flex -my-16">
             <div className="m-auto">
-                <h1>Projects</h1>
+                <h1 className="text-6xl font-medium">Projects</h1>
                 <p>WORK IN PROGRESS</p>
                 <div>
 
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ export default function Resume() {
     return(
         <div className="bg-gradient-to-r from-blue-300 to-blue-50 text-center min-h-screen flex -my-16">
             <div className="m-auto">
-                <h1>Resume</h1>
+                <h1 className="text-6xl font-medium">Resume</h1>
                 <p>WORK IN PROGRESS</p>
             </div>
         </div>
