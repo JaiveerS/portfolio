3c1e0253058dba9bbc8dd16f019e151f0323a03d
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2023-04-17 21:31:44 -0400

    css fix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,11 +6,11 @@ export default function Projects() {
             <div className="m-auto">
                 <h1 className="text-6xl font-medium">Projects</h1>
                 <p>WORK IN PROGRESS</p>
-                <div>
+                <div className="flex flex-col">
                     <a href="http://140.238.147.51/">E-Commerce Website</a>
                     <a href="https://employee-managment-f5252.web.app/login">Smart Employee Management System</a>
                     <a href="http://140.238.155.208/">Blue Collar React App</a>
-                    <a  href="https://jaiveer.netlify.app/">Portfolio</a>
+                    <a href="https://jaiveer.netlify.app/">Portfolio</a>
                 </div>
             </div>
         </div>
