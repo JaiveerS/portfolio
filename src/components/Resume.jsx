7616514d0ee36005818c592844fbd138103a4f45
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2023-06-05 12:30:00 -0400

    fixed resume to use tailwind typography

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,25 +1,11 @@
-import React, { useEffect, useState } from "react";
-import ReactMarkdown from 'react-markdown';
-import markdownPath from "../resume.md";
-import remarkGfm from 'remark-gfm'
+import ResumeContent from "./ResumeContent";
 
 
 export default function Resume() {
-    const [markdown, setMarkdown] = useState("");
-
-    useEffect(() => {
-        fetch(markdownPath)
-          .then(response => response.text())
-          .then(text => setMarkdown(text));
-      }, []);
-
     return(
-        <div className="bg-gradient-to-r from-blue-300 to-blue-50 text-center min-h-screen flex">
-            <div className="m-auto">
-                <h1 className="text-6xl font-medium py-4">Resume</h1>
-                <div className="text-left max-w-2xl bg-white py-4 px-4 rounded-lg">
-                    <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]}/>
-                </div>
+        <div className="bg-gradient-to-r from-blue-300 to-blue-50 min-h-screen bg-gray-50 py-8 flex flex-col justify-center relative overflow-hidden lg:py-12">
+            <div className="relative w-full px-6 py-12 bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-16 lg:pb-28">
+                <ResumeContent />
             </div>
         </div>
     )
---
+++ src/components/ResumeContent.jsx
@@ -0,0 +1,117 @@
+
+
+export default function ResumeContent(){
+    return(
+        <div className="max-w-prose mx-auto lg:text-lg">
+            <h1 class="text-3xl font-bold">Jaiveer Singh</h1>
+            
+            <p className="text-r"><a className="hover:bg-gray-100 hover:text-blue-900" href="mailto:jaiveer_@hotmail.com">jaiveer_@hotmail.com</a> | 647-639-3289 | <a className="hover:bg-gray-100 text-blue-900 underline" href="https://github.com/JaiveerS" target="_blank" rel="noopener noreferrer">github.com/jaiveerS</a> | <a className="hover:bg-gray-100 text-blue-900 underline" href="https://jaiveer.netlify.app">portfolio: jaiveer.netlify.app</a></p>
+            
+            <h2 class="text-2xl font-bold mt-4">Education</h2>
+            
+            <ul className="pl-6">
+            <li>Bachelor of Science in Computer Science <a href="https://lassonde.yorku.ca/">Lassonde School Of Engineering</a> - York University, Toronto ON</li>
+            </ul>
+            
+            <h2 class="text-2xl font-bold mt-4">Projects</h2>
+            
+            <h3 class="text-xl font-bold mt-2">Full Stack e-Commerce Website</h3>
+            
+            <ul className="list-disc pl-6">
+                <div className="px-4 py-2">
+                    <p>Deployment: <a  className="hover:bg-gray-100 text-blue-900 underline" href="http://140.238.147.51" target="_blank" rel="noopener noreferrer">http://140.238.147.51</a></p>
+                    <p>Source Code: <a  className="hover:bg-gray-100 text-blue-900 underline" href="https://github.com/JaiveerS/ecommerce-store-backend" target="_blank" rel="noopener noreferrer">github.com/JaiveerS/ecommerce-store-backend</a></p>
+                </div>
+                <li>Designed and implemented the front-end using ReactJS and Tailwind CSS, ensuring a responsive and user-friendly interface.</li>
+                <li>Developed the back-end using the Spring framework, following best practices for modular and scalable code.</li>
+                <li>Integrated JWTs for user authentication and authorization, securing sensitive user information and ensuring a secure login experience.</li>
+                <li>Implemented a RESTful API, providing well-designed endpoints for the front-end to interact with the back-end.</li>
+                <li>Implemented CRUD operations for managing products, orders, and user profiles, enabling functionalities such as adding, updating, and deleting products, placing orders, and managing user information.</li>
+                <li>Utilized a MySQL database to store and retrieve product information, order details, and user data.</li>
+            </ul>
+            
+            <h3 class="text-xl font-bold mt-4">Smart Employee Management System</h3>
+            
+            <ul className="list-disc pl-6">
+                <div className="px-4 py-2">
+                    <p>Deployment: <a className="hover:bg-gray-100 text-blue-900 underline" href="https://employee-managment-f5252.web.app/login" target="_blank" rel="noopener noreferrer">https://employee-managment-f5252.web.app/login</a></p>
+                    <p>Source Code: <a className="hover:bg-gray-100 text-blue-900 underline" href="https://github.com/JaiveerS/smart-employee-managment-system" target="_blank" rel="noopener noreferrer">github.com/JaiveerS/smart-employee-managment-system</a></p>
+                </div>
+                <li>Collaborated with a team of 5 developers to plan and execute the development of a smart employee management system.</li>
+                <li>Organized regular team meetings, facilitating effective communication and coordination among team members.</li>
+                <li>Assisted in developing project plans, setting milestones, and ensuring timely completion of deliverables.</li>
+                <li>Designed the system architecture using a 3-Tier, MVC, and SOA approach, ensuring scalability, modularity, and maintainability.</li>
+                <li>Prepared comprehensive documentation in the form of UML diagrams, including use cases, sequence diagrams, class diagrams, component diagrams, and deployment diagrams.</li>
+                <li>Created a prototype of the system to showcase its core functionalities and user experience.</li>
+            </ul>
+            
+            <h3 class="text-xl font-bold mt-4">Blue Collar React App</h3>
+            
+            <ul className="list-disc pl-6">
+                <div className="px-4 py-2">
+                    <p>Deployment: <a className="hover:bg-gray-100 text-blue-900 underline" href="http://140.238.155.208/" target="_blank" rel="noopener noreferrer">http://140.238.155.208/</a></p>
+                    <p>Source Code: <a className="hover:bg-gray-100 text-blue-900 underline" href="https://github.com/JaiveerS/blue-collar-app" target="_blank" rel="noopener noreferrer">github.com/JaiveerS/blue-collar-app</a></p>
+                </div>
+                <li>Collaborated with the blue-collar business to understand their requirements and objectives for the website.</li>
+                <li>Designed and developed a user-friendly React application, showcasing the business's information and services in a visually appealing manner.</li>
+                <li>Implemented a contact form using EmailJs, allowing users to submit their contact information directly on the website.</li>
+                <li>Ensured a responsive design, allowing the application to adapt to different screen sizes and devices.</li>
+                <li>Deployed the React application to a virtual machine and used Nginx, making it accessible to users online.</li>
+            </ul>
+            
+            <h3 class="text-xl font-bold mt-4">Ransomware Agent</h3>
+            <ul className="list-disc pl-6">
+                <div className="px-4 py-2">
+                    <p>Source Code: <a className="hover:bg-gray-100 text-blue-900 underline" href="https://github.com/JaiveerS/ransomeware-agent" target="_blank" rel="noopener noreferrer">github.com/JaiveerS/ransomeware-agent</a></p>
+                </div>
+                <li>Developed a ransomware agent with the ability to encrypt and decrypt any given file or folder.</li>
+                <li>Worked in a group of 5 to implement different asymmetric and symmetric cryptographic algorithms (XOR, AES, RC4, RSA, and ECC).</li>
+            </ul>
+            
+            <h2 class="text-2xl font-bold mt-8">Skills</h2>  
+            <div className="flex justify-between">
+                <div>
+                    <h4 class="text-lg font-bold mt-1 px-2">Front-end Development</h4>
+                    
+                    <ul className="list-disc pl-6">
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                        <li>React</li>
+                    </ul>
+                </div>
+                
+                <div>
+                    <h4 class="text-lg font-bold mt-1 px-2">Back-end Development</h4>
+                    
+                    <ul className="list-disc pl-6">
+                        <li>Java</li>
+                        <li>Spring boot</li>
+                        <li>SQL</li>
+                    </ul>
+                </div>
+                
+                <div>
+                    <h4 class="text-lg font-bold mt-2 px-2">Other Skills</h4>
+                    
+                    <ul className="list-disc pl-6">
+                        <li>C</li>
+                        <li>C#</li>
+                        <li>Python</li>
+                        <li>Bash</li>
+                        <li>Git</li>
+                    </ul>
+                </div>
+                
+                <div>
+                    <h3 class="text-xl font-bold mt-4 px-2">Web Servers and Deployment</h3>
+                    
+                    <ul className="list-disc pl-6">
+                        <li>nginx</li>
+                        <li>Linux</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
