5c525d0e3a8a7c9732fab2930c3eeb82f678d9aa
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2024-08-30 11:44:51 -0400

    changed to use iframe

--- src/components/ResumeContent.jsx
+++ src/components/ResumeContent.jsx
@@ -3,8 +3,7 @@ export default function ResumeContent() {
 
     return (
         <div>
-            {/* <iframe title="resume" type="application/pdf" className="w-full h-screen" src='/Jaiveer_Singh_CV.pdf'/> */}
-            <embed title="resume" type="application/pdf"  className="w-full h-screen" src='/Jaiveer_Singh_CV.pdf'/>
+            <iframe title="resume" className="w-full h-screen" src='/Jaiveer_Singh_CV.pdf'/>
         </div>
     );
 }
