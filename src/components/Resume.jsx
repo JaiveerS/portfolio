05c893609fe157cbbd03d886cecf273843bba971
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2024-08-30 11:27:52 -0400

    updated resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,8 +3,8 @@ import ResumeContent from "./ResumeContent";
 
 export default function Resume() {
     return(
-        <div className="bg-gradient-to-r from-blue-300 to-blue-50 min-h-screen bg-gray-50 py-8 flex flex-col justify-center relative overflow-hidden lg:py-12">
-            <div className="relative w-full px-6 py-12 bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-16 lg:pb-28">
+        <div className="bg-gradient-to-r from-blue-300 to-blue-50 min-h-screen bg-gray-50 flex flex-col justify-center relative overflow-hidden">
+            <div className="relative w-full px-6 py-12 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-16 lg:pb-28">
                 <ResumeContent />
             </div>
         </div>
