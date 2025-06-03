192a059dbbe03baacb11976e81357f6d83aea14f
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2025-06-02 21:17:03 -0400

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ export default function Projects() {
             <div className="mx-auto mt-10">
                 <h1 className="text-6xl font-medium">Projects</h1>
                 <div className="flex space-between flex-wrap justify-center p-10 max-w-screen-2xl">
-                    <Project title="E-Commerce Website" link="http://140.238.147.51/" code="https://github.com/JaiveerS/shop-backend-v1"/>
+                    <Project title="E-Commerce Website" link="https://shop.jaiveersingh.com/" code="https://github.com/JaiveerS/shop-backend-v1"/>
                     <Project title="Smart Employee Management System" link="https://employee-managment-f5252.web.app/login" code="https://github.com/JaiveerS/smart-employee-managment-system"/>
                     <Project title="Blue Collar React App" link="http://140.238.155.208/" code="https://github.com/JaiveerS/blue-collar-app"/>
                     <Project title="Portfolio" link="https://jaiveer.netlify.app/" code="https://github.com/JaiveerS/jaiveer-portfolio"/>
